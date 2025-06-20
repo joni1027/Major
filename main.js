62e10d1f14951fdeffdc8d847e36a6034b6c0622
joni1027 <147539212+joni1027@users.noreply.github.com>
2025-06-20 19:03:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//Header scroll color change
+let header = document.querySelector('header');
+
+window.addEventListener('scroll',() => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+// Menu
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');    
+}
+
+// Remove menu on scroll
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
